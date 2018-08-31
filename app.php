950dfc6dd28cb664a7b1fa0922a4dad239fb08da
Angus Tasker <atasker2@gmail.com>
2018-08-31 12:51:12 -0400

    protecting against duplicate rows in users table

--- app.php
+++ app.php
@@ -10,6 +10,7 @@ include 'inc.php';
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
 $accessToken = $_GET['accessToken'];
+$refreshToken = $_GET['refreshToken'];
 
 $api->setAccessToken($accessToken);
 
@@ -18,11 +19,19 @@ $email = $user->email;
 
 $conn = new DB();
 
-$stmt = $conn->db->query("UPDATE users SET email = '$email' WHERE accessToken = '$accessToken';");
+$stmt = $conn->db->query("SELECT * FROM users WHERE email = '$email'");
 
-if ($stmt) {
-    echo "User has been successfully authenticated.</br>" . PHP_EOL;
-    echo "Account information stored in DB.</br>" . PHP_EOL;
-    echo "<b>Email:</b> $email</br>" . PHP_EOL;
-    echo "<b>ID:</b> $user->id" . PHP_EOL;
+$results = $stmt->fetchAll();
+
+if (count($results) >= 1) {
+    echo 'This user has already been authenticated';
+} else {
+    $conn2 = new DB();
+    $stmt2 = $conn2->db->query("INSERT INTO users (email, accessToken, refreshToken) VALUES ('$email', '$accessToken', '$refreshToken')");
+    if ($stmt2) {
+        echo "User has been successfully authenticated.</br>" . PHP_EOL;
+        echo "Account information stored in DB.</br>" . PHP_EOL;
+        echo "<b>Email:</b> $email</br>" . PHP_EOL;
+        echo "<b>ID:</b> $user->id" . PHP_EOL;
+    }
 }
--- callback.php
+++ callback.php
@@ -12,6 +12,7 @@ $session = new SpotifyWebAPI\Session(
     '13ebd10f15714843aea76c5c7259e516',
     '93b62230ebd64bcb8640329caaf9c90d',
     'https://musicintl.herokuapp.com/callback.php'
+    //'http://localhost/MusicIntl/callback.php'
 );
 
 $session->requestAccessToken($_GET['code']);
@@ -19,15 +20,8 @@ $session->requestAccessToken($_GET['code']);
 $accessToken = $session->getAccessToken();
 $refreshToken = $session->getRefreshToken();
 
-$conn = new DB();
-
-$stmt = $conn->db->query("INSERT INTO users (accessToken, refreshToken) VALUES ('$accessToken', '$refreshToken')");
-
-//if ($stmt) {
-//    echo "Successful INSERT of data" . PHP_EOL;
-//}
-
-$url = "app.php?accessToken=$accessToken";
+$url = "app.php?accessToken=$accessToken&refreshToken=$refreshToken";
 
 header("Location: " . $url );
+
 die();
