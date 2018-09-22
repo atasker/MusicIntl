b596647b1618f7a4e15df7a12f1ac73e881b02aa
Angus Tasker <atasker2@gmail.com>
2018-09-22 08:20:39 -0400

    integrate Face API

--- admin/faceDetect.php
+++ admin/faceDetect.php
@@ -10,6 +10,8 @@ include __DIR__ . '/../inc.php';
 
 $face = new FaceDetect("http://img2.timeinc.net/people/i/2014/database/140831/justin-bieber-300.jpg");
 $analyze = $face->analyzeFace();
+print_r($analyze);
+die();
 $gender = $analyze[0]['faceAttributes']['gender'];
 $age = $analyze[0]['faceAttributes']['age'];
 $emotion = $analyze[0]['faceAttributes']['emotion']; // Array
