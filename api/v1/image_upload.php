6b6128376f9ae76e2ff9ef001caba60c909bfb60
Angus Tasker <atasker2@gmail.com>
2018-10-19 16:08:29 -0400

    upload image api

---
+++ api/v1/image_upload.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ATasker
+ * Date: 9/25/18
+ * Time: 2:45 PM
+ */
+
+include __DIR__ . '/../../inc.php';
+
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+switch ($request_method) {
+    case 'POST':
+        if (isset($_POST["encoded_image"])) {
+
+            $unique_id = time();
+            $encoded_image = $_POST["encoded_image"];
+            $upload_directory = __DIR__ . "/../../upload/images/";
+            $upload_path = $upload_directory . $unique_id . ".png";
+
+            // Decode base64 image
+            $encoded_image = str_replace('data:image/png;base64,', '', $encoded_image);
+            $encoded_image = str_replace(' ', '+', $encoded_image);
+            $decoded_image = base64_decode($encoded_image);
+
+            // Save reference to images table
+            $conn = new DB();
+            $stmt = $conn->db->prepare("INSERT INTO image_uploads (name) VALUES (:name)");
+            $stmt->bindValue(':name', $unique_id, PDO::PARAM_STR);
+            $stmt->execute();
+
+            // Save image file to uploads folder
+            file_put_contents($upload_path, $decoded_image);
+
+        }
+        break;
+    default:
+    // Invalid Request Method
+    header("HTTP/1.0 405 Method Not Allowed");
+    break;
+}
