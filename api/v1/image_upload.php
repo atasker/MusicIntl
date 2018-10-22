f63a5d4b05c3c373754fa7e4b63231c62c4491c3
Angus Tasker <atasker2@gmail.com>
2018-10-22 15:03:39 -0400

    upload image api

--- api/v1/image_upload.php
+++ api/v1/image_upload.php
@@ -16,38 +16,31 @@ switch ($request_method) {
     case 'POST':
         if (isset($_POST["encoded_image"])) {
 
-            $received = $_POST["encoded_image"];
-            $length = strlen($received);
-            $return = ["Character count:" => $length];
-            echo json_encode($return);
+            $unique_id = time();
+            $encoded_image = $_POST["encoded_image"];
+            $upload_directory = __DIR__ . "/../../upload/images/";
+            $upload_path = $upload_directory . $unique_id . ".jpg";
 
-//            $unique_id = time();
-//            $encoded_image = $_POST["encoded_image"];
-//            $upload_directory = __DIR__ . "/../../upload/images/";
-//            $upload_path = $upload_directory . $unique_id . ".png";
-//
-//            // Decode base64 image
-//            $encoded_image = str_replace('data:image/png;base64,', '', $encoded_image);
-//            $encoded_image = str_replace(' ', '+', $encoded_image);
-//            $decoded_image = base64_decode($encoded_image);
-//
-//            // Save reference to images table
-//            $conn = new DB();
-//            $stmt = $conn->db->prepare("INSERT INTO image_uploads (name) VALUES (:name)");
-//            $stmt->bindValue(':name', $unique_id, PDO::PARAM_STR);
-//            $stmt->execute();
-//
-//            // Save image file to uploads folder
-//            file_put_contents($upload_path, $decoded_image);
-//
-//            // Send unique ID back to Xcode for use in main API
-//            $return_id['image_id'] = $unique_id;
+            // Decode base64 image
+            $encoded_image = str_ireplace('data:image/jpg;base64,', '', $encoded_image);
+            $encoded_image = str_ireplace('data:image/jpeg;base64,', '', $encoded_image);
+            $encoded_image = str_replace(' ', '+', $encoded_image);
+            $decoded_image = base64_decode($encoded_image);
 
-        } else {
-            $return = ["POSTSET" => "No"];
-            echo json_encode($return);
+            // Save reference to images table
+            $conn = new DB();
+            $stmt = $conn->db->prepare("INSERT INTO image_uploads (name) VALUES (:name)");
+            $stmt->bindValue(':name', $unique_id, PDO::PARAM_STR);
+            $stmt->execute();
+
+            // Save image file to uploads folder
+            file_put_contents($upload_path, $decoded_image);
+
+            // Send unique ID back to Xcode for use in main API
+            $return_id['image_id'] = $unique_id;
+
         }
-        //echo json_encode($return_id);
+        echo json_encode($return_id);
         break;
     default:
     // Invalid Request Method
