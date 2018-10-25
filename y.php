c804dc315dc12201ce0074e18724032a731915ea
Angus Tasker <atasker2@gmail.com>
2018-10-24 23:48:38 -0400

    add sendgrid for email support

--- y.php
+++ y.php
@@ -1,22 +1,43 @@
 <?php
 
-// Test script for image uploading from iOS
+include "inc.php";
 
-$url = "http://musicintl.herokuapp.com/api/v1/image_upload.php";
-$local = "localhost/musicintl/api/v1/image_upload.php";
+// Test script
 
-$param = array(
-    'encoded_image' => "some_random_string==//dfdf//+l+jjkki"
-);
+// Image uploading API
 
-$postvars = http_build_query($param);
+//$url = "http://musicintl.herokuapp.com/api/v1/image_upload.php";
+//$local = "localhost/musicintl/api/v1/image_upload.php";
+//
+//$param = array(
+//    'encoded_image' => "some_random_string==//dfdf//+l+jjkki"
+//);
+//
+//$postvars = http_build_query($param);
+//
+//$ch = curl_init();
+//
+//curl_setopt($ch, CURLOPT_URL, $url);
+//curl_setopt($ch, CURLOPT_POST, count($param));
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+//
+//$result = curl_exec($ch);
+//
+//curl_close($ch);
 
-$ch = curl_init();
+// Send email (to be implemented with use sessions and other admin alerts)
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, count($param));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+$from = new SendGrid\Email(null, "robot@caravan.com");
+$subject = "Test email from Caravan using SendGrid";
+$to = new SendGrid\Email(null, "atasker2@gmail.com");
+$content = new SendGrid\Content("text/plain", "Message body.");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$result = curl_exec($ch);
+$sendgrid_api_key = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($sendgrid_api_key);
 
-curl_close($ch);
+$response = $sg->client->mail()->send()->post($mail);
+
+//echo $response->statusCode();
+//echo $response->headers();
+//echo $response->body();
